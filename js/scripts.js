3530c98896cbece0d26802c58ffef92aabd71138
Student <example@example.com>
2019-11-05 11:36:02 -0800

    add two options for email addresses to be entered, and our DOM currently shows both of them on the same line

--- js/scripts.js
+++ js/scripts.js
@@ -40,13 +40,15 @@ AddressBook.prototype.deleteContact = function(id) {
 }
 
 //business logic for Contact object
-function Contact (firstName, lastName, phoneNumber, address, emailAddress) {
+function Contact (firstName, lastName, phoneNumber, address, emailWork, emailPersonal) {
   this.firstName = firstName,
   this.lastName = lastName,
   this.phoneNumber = phoneNumber,
   this.address = address,
-  this.emailAddress = emailAddress
+  this.emailWork = emailWork,
+  this.emailPersonal = emailPersonal
 }
+
 
 Contact.prototype.fullName = function () {
   return this.firstName + " " + this.lastName;
@@ -72,7 +74,8 @@ function showContact(contactId) {
   $(".last-name").html(contact.lastName);
   $(".phone-number").html(contact.phoneNumber);
   $(".address").html(contact.address);
-  $(".email-address").html(contact.emailAddress);
+  $(".email-work").html(contact.emailWork);
+  $(".email-personal").html(contact.emailPersonal);
   var buttons = $("#buttons");
   buttons.empty();
   buttons.append("<button class='deleteButton' id=" + + contact.id + ">Delete</button>");
@@ -98,15 +101,18 @@ $(document).ready(function() {
     var inputtedLastName = $("input#new-last-name").val();
     var inputtedPhoneNumber = $("input#new-phone-number").val();
     var inputtedAddress= $("input#new-address").val();
-    var inputtedEmailAddress = $("input#new-email-address").val();
+    var inputtedEmailWork = $("input#new-email-work").val();
+    var inputtedEmailPersonal = $("input#new-email-personal").val();
+
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
     $("input#new-phone-number").val("");
     $("input#new-address").val("");
-    $("input#new-email-address").val("");
+    $("input#new-email-work").val("");
+    $("input#new-email-personal").val("");
 
-    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedAddress, inputtedEmailAddress);
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedAddress, [inputtedEmailWork + inputtedEmailPersonal]); //built array within an arguements. Issue arises when in the DOM by having both email on one line. 
     addressBook.addContact(newContact);
     console.log(addressBook.contacts);
     displayContactDetails(addressBook);
